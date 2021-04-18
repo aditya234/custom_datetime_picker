a2dbf0d6e3b340fbd916e007366090e96da9d062
Aditya Soni <adityasoni5859@gmail.com>
2021-04-18 08:51:45 +0530

    getting calender in form of a map instead of a list of datetimes

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,3 +1,5 @@
+import 'package:datetime_widget/utils/constants.dart';
+
 extension DateTimeHelpers on DateTime {
   bool isToday() {
     DateTime today = DateTime.now();
@@ -7,11 +9,27 @@ extension DateTimeHelpers on DateTime {
     return false;
   }
 
-  List<DateTime> getDaysInBetween(DateTime endDate) {
-    List<DateTime> days = [];
+  Map<int, Map<String, List<int>>> getDaysInBetween(DateTime endDate) {
+    Constants constants = Constants();
+    Map<int, Map<String, List<int>>> calender = {};
+    calender[this.year] = {
+      constants.monthMapping[this.month]: [this.day],
+    };
     for (int i = 0; i <= endDate.difference(this).inDays; i++) {
-      days.add(this.add(Duration(days: i)));
+      // days.add(this.add(Duration(days: i)));
+      DateTime date = this.add(Duration(days: i));
+      if (calender.containsKey(date.year)) {
+        if (calender[date.year].containsKey(constants.monthMapping[this.month])) {
+          calender[date.year][constants.monthMapping[date.month]].add(date.day);
+        } else {
+          calender[date.year][constants.monthMapping[date.month]] = [date.day];
+        }
+      } else {
+        calender[date.year] = {
+          constants.monthMapping[date.month]: [date.day],
+        };
+      }
     }
-    return days;
+    return calender;
   }
 }
--- lib/widgets/custom_datepicker.dart
+++ lib/widgets/custom_datepicker.dart
@@ -17,12 +17,19 @@ class _CustomDatePickerState extends State<CustomDatePicker> {
   int selectedDateIndex;
   Orientation orientation;
   Constants constants = Constants();
-  List<DateTime> days = [];
+  Map<int, Map<String, List<int>>> calender;
+  List<int> days = [];
+  List<String> months = [];
+  List<int> years = [];
 
   @override
   void initState() {
     super.initState();
-    days = widget.startDate.getDaysInBetween(widget.endDate);
+    calender = widget.startDate.getDaysInBetween(widget.endDate);
+    years = calender.keys.toList();
+    // Since initially first year is selected, we get the months in first year, and days in that month
+    months = calender[years.first].keys.toList();
+    days = calender[years.first][months.first];
   }
 
   @override
@@ -31,65 +38,76 @@ class _CustomDatePickerState extends State<CustomDatePicker> {
     orientation = MediaQuery.of(context).orientation;
   }
 
+  getCalenderDateTimes() {}
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 350,
       padding: EdgeInsets.all(8),
-      child: GridView.builder(
-        itemCount: days.length,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: (orientation == Orientation.portrait) ? 3 : 4),
-        itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              setState(() {
-                selectedDateIndex = index;
-                widget.onSelect(days[selectedDateIndex]);
-              });
-            },
-            child: Card(
-              color: selectedDateIndex == index
-                  ? Colors.blueAccent
-                  : days[index].isToday()
-                      ? Colors.grey[300]
-                      : Colors.white,
-              child: Container(
-                padding: EdgeInsets.all(2),
-                child: GridTile(
-                  header: Center(
-                    child: Text(
-                      "${days[index].year}",
-                      style: TextStyle(
-                        fontSize: 8,
-                        color: (selectedDateIndex == index) ? Colors.white : Colors.black,
-                      ),
-                    ),
-                  ),
-                  footer: Center(
-                    child: Text(
-                      constants.dayNames[days[index].weekday],
-                      style: TextStyle(
-                        fontSize: 8,
-                        color: (selectedDateIndex == index) ? Colors.white : Colors.black,
-                      ),
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "${constants.monthMapping[days[index].month]} ${days[index].day}",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: (selectedDateIndex == index) ? Colors.white : Colors.black,
-                          fontSize: 12),
-                    ),
-                  ), //just for testing, will fill with image later
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+
+            ],
+          )
+        ],
+      )
+      // child: GridView.builder(
+      //   itemCount: days.length,
+      //   gridDelegate:
+      //       SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: (orientation == Orientation.portrait) ? 3 : 4),
+      //   itemBuilder: (context, index) {
+      //     return InkWell(
+      //       onTap: () {
+      //         setState(() {
+      //           selectedDateIndex = index;
+      //           widget.onSelect(days[selectedDateIndex]);
+      //         });
+      //       },
+      //       child: Card(
+      //         color: selectedDateIndex == index
+      //             ? Colors.blueAccent
+      //             : days[index].isToday()
+      //                 ? Colors.grey[300]
+      //                 : Colors.white,
+      //         child: Container(
+      //           padding: EdgeInsets.all(2),
+      //           child: GridTile(
+      //             header: Center(
+      //               child: Text(
+      //                 "${days[index].year}",
+      //                 style: TextStyle(
+      //                   fontSize: 8,
+      //                   color: (selectedDateIndex == index) ? Colors.white : Colors.black,
+      //                 ),
+      //               ),
+      //             ),
+      //             footer: Center(
+      //               child: Text(
+      //                 constants.dayNames[days[index].weekday],
+      //                 style: TextStyle(
+      //                   fontSize: 8,
+      //                   color: (selectedDateIndex == index) ? Colors.white : Colors.black,
+      //                 ),
+      //               ),
+      //             ),
+      //             child: Center(
+      //               child: Text(
+      //                 "${constants.monthMapping[days[index].month]} ${days[index].day}",
+      //                 style: TextStyle(
+      //                     fontWeight: FontWeight.bold,
+      //                     color: (selectedDateIndex == index) ? Colors.white : Colors.black,
+      //                     fontSize: 12),
+      //               ),
+      //             ), //just for testing, will fill with image later
+      //           ),
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
     );
   }
 }
